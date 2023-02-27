ab3c3555a74639772b413a4baf1bb7a4ff09ee39
JWittmeyer <jens.wittmeyer@onetask.ai>
2023-02-27 13:03:23 +0100

    Changes to PlanResponse

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, responses, status
 from typing import Tuple
 
 
@@ -20,15 +19,17 @@ app = FastAPI()
 
 
 @app.post("/tokenize_record")
-def tokenize_record(request: Request) -> Tuple[int, str]:
+def tokenize_record(request: Request) -> responses.PlainTextResponse:
     session_token = general.get_ctx_token()
     tokenization_manager.tokenize_record(request.project_id, request.record_id)
     general.remove_and_refresh_session(session_token)
-    return 200, ""
+    return responses.PlainTextResponse(status_code=status.HTTP_200_OK)
 
 
 @app.post("/tokenize_calculated_attribute")
-def tokenize_record(request: AttributeTokenizationRequest) -> Tuple[int, str]:
+def tokenize_record(
+    request: AttributeTokenizationRequest,
+) -> responses.PlainTextResponse:
     session_token = general.get_ctx_token()
     task_manager.start_tokenization_task(
         request.project_id,
@@ -37,61 +38,62 @@ def tokenize_record(request: AttributeTokenizationRequest) -> Tuple[int, str]:
         request.attribute_id,
     )
     general.remove_and_refresh_session(session_token)
-    return 200, ""
+    return responses.PlainTextResponse(status_code=status.HTTP_200_OK)
 
 
 @app.post("/tokenize_project")
-def tokenize_project(request: Request) -> Tuple[int, str]:
+def tokenize_project(request: Request) -> responses.PlainTextResponse:
     session_token = general.get_ctx_token()
     task_manager.start_tokenization_task(
         request.project_id, request.user_id, enums.TokenizationTaskTypes.PROJECT.value
     )
     general.remove_and_refresh_session(session_token)
-    return 200, ""
+    return responses.PlainTextResponse(status_code=status.HTTP_200_OK)
 
 
 # rats = record_attribute_token_statistics
 @app.post("/create_rats")
-def create_rats(request: RatsRequest) -> Tuple[int, str]:
+def create_rats(request: RatsRequest) -> responses.PlainTextResponse:
     session_token = general.get_ctx_token()
     attribute_id = request.attribute_id if request.attribute_id != "" else None
     task_manager.start_rats_task(request.project_id, request.user_id, attribute_id)
     general.remove_and_refresh_session(session_token)
-    return 200, ""
+    return responses.PlainTextResponse(status_code=status.HTTP_200_OK)
 
 
 @app.put("/tokenize_project_for_migration/{project_id}")
-def tokenize_project_no_use(project_id: str) -> int:
+def tokenize_project_no_use(project_id: str) -> responses.PlainTextResponse:
     user_id = util.get_migration_user()
-    return task_manager.start_tokenization_task(
+    status_code = task_manager.start_tokenization_task(
         project_id, user_id, enums.TokenizationTaskTypes.PROJECT.value
     )
+    return responses.PlainTextResponse(status_code=status_code)
 
 
 @app.post("/reupload_docbins")
-def reupload_docbins(request: ReuploadDocbins) -> Tuple[int, str]:
+def reupload_docbins(request: ReuploadDocbins) -> responses.PlainTextResponse:
     session_token = general.get_ctx_token()
     util.reupload_docbins(request.project_id)
     general.remove_and_refresh_session(session_token)
-    return 200, ""
+    return responses.PlainTextResponse(status_code=status.HTTP_200_OK)
 
 
 @app.post("/save_tokenizer")
-def save_tokenizer_as_pickle(request: SaveTokenizer) -> Tuple[int, str]:
+def save_tokenizer_as_pickle(request: SaveTokenizer) -> responses.PlainTextResponse:
     tokenizer_handler.save_tokenizer_as_pickle(request.config_string, request.overwrite)
-    return 200, ""
+    return responses.PlainTextResponse(status_code=status.HTTP_200_OK)
 
 
 @app.exception_handler(Exception)
-async def error_handler() -> JSONResponse:
+async def error_handler() -> responses.PlainTextResponse:
     general.rollback()
-    return JSONResponse(
-        status_code=400,
-        content={"message": "Oops! Something went wrong. Database gets a rollback..."},
+    return responses.PlainTextResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content="Oops! Something went wrong. Database gets a rollback...",
     )
 
 
 @app.put("/config_changed")
-def config_changed() -> int:
+def config_changed() -> responses.PlainTextResponse:
     config_handler.refresh_config()
-    return 200
+    return responses.PlainTextResponse(status_code=status.HTTP_200_OK)

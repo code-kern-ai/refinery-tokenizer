421cbe33c5c6136340527448f00b7d235a064af2
anmarhindi <anmar.hindi@kern.ai>
2024-11-05 17:31:45 +0100

    Adjust config route

--- handler/config_handler.py
+++ handler/config_handler.py
@@ -6,7 +6,7 @@ from misc import daemon
 __config = None
 
 # meant as a const value since env variables will be removed at some point
-REQUEST_URL = "http://refinery-gateway/api/v1/misc/full_config"
+REQUEST_URL = "http://refinery-gateway:80/full_config"
 
 
 def __get_config() -> Dict[str, Any]:

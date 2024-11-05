419991b2e8052fd0d696f810889d88083569d814
anmarhindi <anmar.hindi@kern.ai>
2024-11-05 15:38:12 +0100

    remove config service

--- handler/config_handler.py
+++ handler/config_handler.py
@@ -6,7 +6,8 @@ from misc import daemon
 __config = None
 
 # meant as a const value since env variables will be removed at some point
-REQUEST_URL = "http://refinery-config:80/full_config"
+# REQUEST_URL = "http://refinery-config:80/full_config"
+REQUEST_URL = "http:///refinery-gateway/api/v1/misc/full_config"
 
 
 def __get_config() -> Dict[str, Any]:

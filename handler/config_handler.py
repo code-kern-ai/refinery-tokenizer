0dcd3f3525ca395850bd3da1a043e9c3c94d30ec
anmarhindi <anmar.hindi@kern.ai>
2024-11-05 16:01:56 +0100

    edit

--- handler/config_handler.py
+++ handler/config_handler.py
@@ -6,7 +6,6 @@ from misc import daemon
 __config = None
 
 # meant as a const value since env variables will be removed at some point
-# REQUEST_URL = "http://refinery-config:80/full_config"
 REQUEST_URL = "http://refinery-gateway/api/v1/misc/full_config"
 
 

db817cce5ba4e995baee6f4cd9809b20ec36800e
JWittmeyer <jens.wittmeyer@kern.ai>
2025-02-03 12:03:34 +0100

    Adds tokenization for llm attribute

--- controller/tokenization_manager.py
+++ controller/tokenization_manager.py
@@ -224,7 +224,10 @@ def __get_chunk_size_and_progress_per_chunk(full_count: int) -> Tuple[int, float
 def __check_attribute_is_text(attribute_item: Attribute) -> None:
     if not attribute_item:
         raise Exception("Attribute does not exist.")
-    if attribute_item.data_type != enums.DataTypes.TEXT.value:
+    if (
+        attribute_item.data_type != enums.DataTypes.TEXT.value
+        and attribute_item.data_type != enums.DataTypes.LLM_RESPONSE.value
+    ):
         raise Exception("Attribute is not of type text.")
 
 
